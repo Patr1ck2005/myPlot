1539292ea7009b286d1979709e44229704a69b33
Wang Keren <144989952+Patr1ck2005@users.noreply.github.com>
2025-03-29 20:04:11 +0800

    整合各类绘图模式

--- plot_3D/core/plot_3D_params_space.py
+++ plot_3D/core/plot_3D_params_space.py
@@ -6,10 +6,14 @@ import pandas as pd
 def plot_3D_params_space(
         data,
         save_label,
+        selected_params,
+        cmap,
+        zlim=None,
+        norm_zlim=None,
 ):
     # 准备绘图
     plt.rcParams['font.size'] = 16
-    fig = plt.figure(figsize=(8, 12))
+    fig = plt.figure(figsize=(12, 12))
     ax = fig.add_subplot(111, projection='3d')
 
     # 提取频率并归一化
@@ -17,27 +21,26 @@ def plot_3D_params_space(
     freq_real = [freq.real for freq in frequencies]  # 实部用于颜色映射
     freq_imag = [freq.imag for freq in frequencies]  # 虚部用于透明度
 
-    freq_min, freq_max = 90, 120
-    freq_length = freq_max-freq_min
-
-    freq_im_min = 0.0
-    freq_im_max = 0.2
-    freq_im_length = freq_im_max-freq_im_min
+    freq_min, freq_max = 92, 120
+    # freq_length = freq_max-freq_min
+    # freq_im_min = 0.0
+    # freq_im_max = 0.2
+    # freq_im_length = freq_im_max-freq_im_min
 
     # 创建颜色映射对象
-    refreq_norm = plt.Normalize(vmin=freq_min, vmax=freq_max)
-    imfreq_norm = plt.Normalize(vmin=freq_im_min, vmax=freq_im_max)
+    # refreq_norm = plt.Normalize(vmin=freq_min, vmax=freq_max)
+    # imfreq_norm = plt.Normalize(vmin=freq_im_min, vmax=freq_im_max)
+    norm = plt.Normalize(vmin=norm_zlim[0], vmax=norm_zlim[1])
     # cmap = plt.get_cmap('twilight')
-    cmap = plt.get_cmap('rainbow')
-    selected_params = 0.0
+    cmap = plt.get_cmap(cmap)
 
     # 选择坐标和频率数据进行绘制
     for i, d in enumerate(data):
         buffer, a, k1, freq, freq_real, Q, S_air_prop = d
-        if k1 != selected_params:
+        if k1 != selected_params['k1']:
             print('k1 skip')
             continue
-        if Q < 100:
+        if Q < selected_params['Qlim'][1]:
             print('Q skip')
             continue
         # elif S_air_prop > 10:
@@ -57,7 +60,8 @@ def plot_3D_params_space(
 
         # 频率的实部和虚部影响颜色和透明度
         # color = cmap(refreq_norm(freq_re))
-        color = cmap(imfreq_norm(freq_im))
+        # color = cmap(imfreq_norm(freq_im))
+        color = cmap(norm(z))
         # alpha = np.clip(30/Q/2, 0, 1)  # 透明度由虚部控制
         alpha = 1  # 透明度由虚部控制
 
@@ -67,6 +71,8 @@ def plot_3D_params_space(
         else:
             ax.scatter(buffer, a, z, color=color, alpha=alpha, s=10)
 
+    if zlim is not None:
+        ax.set_zlim(zlim)
     # # 创建X和Z的网格数据
     # y = np.linspace(0, 0.16, 10)  # X的范围
     # z = np.linspace(0, 0.3, 10)  # Z的范围
@@ -100,15 +106,14 @@ def plot_3D_params_space(
     azim_angle = -60+90+75
     # azim_angle = -60+90+120
     ax.view_init(elev=30, azim=azim_angle)
-    # ax.view_init(elev=30, azim=azim_angle)
 
     # # 添加颜色条
-    # sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
-    # sm.set_array([])  # 空数组用于生成颜色条
-    # cbar = plt.colorbar(sm, ax=ax)
-    # cbar.set_label('Frequency (THz)')
-
-    plt.savefig(f'./rsl/3D-{cmap.name}-{"EP_band"}-k1{selected_params}-azim{azim_angle}-{save_label}.png', dpi=500, bbox_inches='tight', pad_inches=0.3, transparent=True)
-    plt.savefig(f'./rsl/3D-{cmap.name}-{"EP_band"}-k1{selected_params}-azim{azim_angle}-{save_label}.svg', bbox_inches='tight', pad_inches=0.3, transparent=True)
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+    sm.set_array([])  # 空数组用于生成颜色条
+    cbar = plt.colorbar(sm, ax=ax, shrink=0.5, aspect=10, pad=0.)
+    cbar.set_label('Frequency (THz)')
+    params_label = [f'{k}={v}' for k, v in selected_params.items()]
+    plt.savefig(f'./rsl/3D-{cmap.name}-{"EP_band"}-{params_label}-azim{azim_angle}-{save_label}.png', dpi=500, bbox_inches='tight', pad_inches=0.3, transparent=True)
+    plt.savefig(f'./rsl/3D-{cmap.name}-{"EP_band"}-{params_label}-azim{azim_angle}-{save_label}.svg', bbox_inches='tight', pad_inches=0.3, transparent=True)
     # plt.show()
 
--- plot_3D/plot_3D-EP_band.py
+++ plot_3D/plot_3D-EP_band.py
@@ -5,9 +5,29 @@ from core.plot_3D_params_space import plot_3D_params_space
 if __name__ == '__main__':
 
     # 载入数据并调用函数
-    data_path = './data/EP_band-tgrating_lowloss.csv'
+    # data_path = './data/EP_band-tgrating_lowloss.csv'
+    # data = pd.read_csv(data_path, sep='\t').to_numpy()
+    # plot_3D_params_space(
+    #     data,
+    #     save_label=data_path.split('/')[-1].split('.')[0],
+    #     selected_params={
+    #         'k1': 0.02,
+    #         'Qlim': (0, 100)
+    #     },
+    #     zlim=[0, 0.3],
+    # )
+
+    # 载入数据并调用函数
+    data_path = './data/EP_band-tgrating_details.csv'
     data = pd.read_csv(data_path, sep='\t').to_numpy()
     plot_3D_params_space(
         data,
-        save_label=data_path.split('/')[-1].split('.')[0]
+        save_label=data_path.split('/')[-1].split('.')[0],
+        selected_params={
+            'k1': 0.08,
+            'Qlim': (0, 30)
+        },
+        zlim=[0.4, 1.5],
+        norm_zlim=[0.4, 1.0],
+        cmap='twilight',
     )
